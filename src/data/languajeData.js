d0907063b304212746e618a6c4afe0eede98a0a1
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-17 03:22:00 -0400

    another url

--- src/data/languajeData.js
+++ src/data/languajeData.js
@@ -3,99 +3,99 @@ export const titleData = ["Languages", "Testing", "Front End", "Back End"];
 export const languajeData = [
   {
     text: "JavaScript",
-    src: 'public/Tech Stack/b (12).png',
+    src: '/TechStack/b (12).png',
   },
   {
     text: "TypeScript",
-    src: "public/Tech Stack/b (7).png",
+    src: "/TechStack/b (7).png",
   },
   {
     text: "Python",
-    src: "public/Tech Stack/b (22).png",
+    src: "/TechStack/b (22).png",
   },
   {
     text: "C#",
-    src: "public/Tech Stack/b (3).png",
+    src: "/TechStack/b (3).png",
   },
 ];
 
 export const FrontEndData = [
   {
     text: "Next.js",
-    src: "public/Tech Stack/b (13).png",
+    src: "/TechStack/b (13).png",
   },
   {
     text: "React",
-    src: "public/Tech Stack/b (4).png",
+    src: "/TechStack/b (4).png",
   },
   {
     text: "Angular",
-    src: "public/Tech Stack/b (20).png",
+    src: "/TechStack/b (20).png",
   },
   {
     text: "Astro",
-    src: "public/Tech Stack/b (2).png",
+    src: "/TechStack/b (2).png",
   },
   {
     text: "Tailwind",
-    src: "public/Tech Stack/b (19).png",
+    src: "/TechStack/b (19).png",
   },
   {
     text: "Bootstrap",
-    src: "public/Tech Stack/b (6).png",
+    src: "/TechStack/b (6).png",
   },
   {
     text: "Redux",
-    src: "public/Tech Stack/b (21).png",
+    src: "/TechStack/b (21).png",
   },
   {
     text: "Figma",
-    src: "public/Tech Stack/b (18).png",
+    src: "/TechStack/b (18).png",
   },
 ];
 
 export const BackEndData = [
   {
     text: "Node.js",
-    src: "public/Tech Stack/b (17).png",
+    src: "/TechStack/b (17).png",
   },
   {
     text: "Express",
-    src: "public/Tech Stack/b (1).png",
+    src: "/TechStack/b (1).png",
   },
   {
     text: "Asp .Net",
-    src: "public/Tech Stack/b (16).png",
+    src: "/TechStack/b (16).png",
   },
   {
     text: "SQL",
-    src: "public/Tech Stack/b (15).png",
+    src: "/TechStack/b (15).png",
   },
   {
     text: "MySQL",
-    src: "public/Tech Stack/b (14).png",
+    src: "/TechStack/b (14).png",
   },
   {
     text: "Git",
-    src: "public/Tech Stack/b (23).png",
+    src: "/TechStack/b (23).png",
   },
   {
     text: "Jira",
-    src: "public/Tech Stack/b (11).png",
+    src: "/TechStack/b (11).png",
   },
 ];
 
 export const TestingData = [
   {
     text: "Jest",
-    src: "public/Tech Stack/b (5).png",
+    src: "/TechStack/b (5).png",
   },
   {
     text: "React-Testing",
-    src: "public/Tech Stack/b (8).png",
+    src: "/TechStack/b (8).png",
   },
   {
     text: "Cypress",
-    src: "public/Tech Stack/b (9).png",
+    src: "/TechStack/b (9).png",
   },
 ];
