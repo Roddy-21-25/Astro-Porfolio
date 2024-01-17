2288e8ca06713f2c62d749179f45db48e0fce36b
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-17 02:59:55 -0400

    first commit

---
+++ src/data/languajeData.js
@@ -0,0 +1,101 @@
+export const titleData = ["Languages", "Testing", "Front End", "Back End"];
+
+export const languajeData = [
+  {
+    text: "JavaScript",
+    src: "b (12).png",
+  },
+  {
+    text: "TypeScript",
+    src: "b (7).png",
+  },
+  {
+    text: "Python",
+    src: "b (22).png",
+  },
+  {
+    text: "C#",
+    src: "b (3).png",
+  },
+];
+
+export const FrontEndData = [
+  {
+    text: "Next.js",
+    src: "b (13).png",
+  },
+  {
+    text: "React",
+    src: "b (4).png",
+  },
+  {
+    text: "Angular",
+    src: "b (20).png",
+  },
+  {
+    text: "Astro",
+    src: "b (2).png",
+  },
+  {
+    text: "Tailwind",
+    src: "b (19).png",
+  },
+  {
+    text: "Bootstrap",
+    src: "b (6).png",
+  },
+  {
+    text: "Redux",
+    src: "b (21).png",
+  },
+  {
+    text: "Figma",
+    src: "b (18).png",
+  },
+];
+
+export const BackEndData = [
+  {
+    text: "Node.js",
+    src: "b (17).png",
+  },
+  {
+    text: "Express",
+    src: "b (1).png",
+  },
+  {
+    text: "Asp .Net",
+    src: "b (16).png",
+  },
+  {
+    text: "SQL",
+    src: "b (15).png",
+  },
+  {
+    text: "MySQL",
+    src: "b (14).png",
+  },
+  {
+    text: "Git",
+    src: "b (23).png",
+  },
+  {
+    text: "Jira",
+    src: "b (11).png",
+  },
+];
+
+export const TestingData = [
+  {
+    text: "Jest",
+    src: "b (5).png",
+  },
+  {
+    text: "React-Testing",
+    src: "b (8).png",
+  },
+  {
+    text: "Cypress",
+    src: "b (9).png",
+  },
+];
---
+++ src/data/projectsData.js
@@ -0,0 +1,61 @@
+export const ProjectsData = [
+  {
+    src: "/Projects/ford mustang shop.png",
+    name: "Ford Mustang Shop",
+    stack: "Next JS / MySQL / Tailwind / UX/UI / Axios / Prisma",
+    link: "https://ford-mustang-lover-shop-next-js.vercel.app",
+    github:
+      "https://github.com/Roddy-21-25/BASE-ford-mustang-lover-shop-next-js",
+  },
+  {
+    src: "/Projects/litbull.png",
+    name: "Litbull",
+    stack: "Angular / Typescript / Bootstrap / Asp Net Web Api",
+    link: "https://litbull-dog-info.netlify.app",
+    github: "https://github.com/Roddy-21-25/Litbull-Angular-Front",
+  },
+  {
+    src: "/Projects/EnglishWords.png",
+    name: "English Words",
+    stack: "React / Redux / Axios / Asp Net Web Api / Python / Tailwind Css",
+    link: "https://english-words-beta-v1.netlify.app",
+    github: "https://github.com/Roddy-21-25/English-Words-V1-Deploy",
+  },
+  {
+    src: "/Projects/ford mustang api.png",
+    name: "Ford Mustang Api",
+    stack:
+      "React / Axios / Redux / Tailwind / Node.js / Express / MySQL / Figma ",
+    link: "https://ford-mustangs-api.netlify.app",
+    github:
+      "https://github.com/Roddy-21-25/Ford-Mustang-Api---ReactJS---NodeJs-ExpressJS",
+  },
+  {
+    src: "/Projects/ecommerce.png",
+    name: "E-commerce",
+    stack: "JS / HTML / CSS / Bootstrap",
+    link: "https://front-end-wed-desing-e-commerce.netlify.app",
+    github: "https://github.com/Roddy-21-25/Front-End-Wed-Desing-e-commerce-",
+  },
+  {
+    src: "/Projects/monex.png",
+    name: "Monex",
+    stack: "JS / HTML / CSS / Bootstrap",
+    link: "https://exchange-monex.netlify.app",
+    github: "https://github.com/Roddy-21-25/Monex-2.0",
+  },
+  {
+    src: "/Projects/Vidclip.png",
+    name: "VidClip",
+    stack: "Angular / Asp.Net Core / Bootstrap",
+    link: "https://vibclip-video-generator.netlify.app",
+    github: "https://github.com/Roddy-21-25/vidClip-front-end-angular",
+  },
+  {
+    src: "/Projects/culturaTour.png",
+    name: "CulturaTour",
+    stack: "Angular / Asp.Net / SQL Server / Bootstrap",
+    link: "https://culturatour.netlify.app",
+    github: "https://github.com/Roddy-21-25/Angular---Front-end---CulturaTour",
+  },
+];
