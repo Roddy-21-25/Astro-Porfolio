230c8a7d27c06e42958a81899bded3d64a4c972c
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-02-02 22:29:40 -0400

    New Buttons | New Experience

---
+++ src/data/CvData.js
@@ -0,0 +1,12 @@
+export const CVs = [
+  {
+    title: "CV - English Version",
+    href: "/Home/Full Stack En CV.pdf",
+    downloadText: "Full Stack En CV",
+  },
+  {
+    title: "CV - Spanish Version",
+    href: "/Home/Full Stack Es CV.pdf",
+    downloadText: "Full Stack Es CV",
+  },
+];
